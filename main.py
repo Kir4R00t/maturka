4d671cf66c9f0b1a45d89735d442818104caa0d6
Przemysław Sadowski <miki4sa@interia.pl>
2023-02-22 19:57:22 +0100

    Dodanie komentarzy

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ file2 = open("dane2.txt", "r")
 liczby1 = []
 liczby2 = []
 
+# Dodanie wartości do listy 
+
 for line in file1:
     liczby1.append(line.strip())
 
 for line in file2:
     liczby2.append(line.strip())
+
+# Zamiana list na dwuwymiarową tablice 
 
 for i in range(len(liczby1)):
     liczby1[i] = liczby1[i].split()
@@ -20,10 +24,11 @@ for i in range(len(liczby2)):
     liczby2[i] = [int(x) for x in liczby2[i]]
     liczby2[i].sort()
 
-print(liczby1)
-print(liczby2)
+# Stworzenie trzeciej listy na scalone i posortowane dane z dwóch list
 
 liczby_ale_posortowane = []
+
+# Algorytm sortowania
 
 for i in range(len(liczby1)):
     a = b = 0
@@ -37,9 +42,11 @@ for i in range(len(liczby1)):
     liczby_ale_posortowane += liczby1[i][a:]
     liczby_ale_posortowane += liczby2[i][b:]
 
+# Wyświetlenie posortowanych danych w 20 wyrazowych wierszach 
+
 for i, liczba in enumerate(liczby_ale_posortowane):
     print(liczba, end=" ")
-    if (i+1) % 10 == 0:
+    if (i+1) % 20 == 0:
         print("\n", end="")
 
 file1.close()
