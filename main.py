070a20b0848859cf81962cb1c4954ebc6c316c4a
Przemysław Sadowski <miki4sa@interia.pl>
2023-02-22 19:43:46 +0100

    prawie działa

--- main.py
+++ main.py
@@ -12,22 +12,43 @@ for line in file2:
 
 for i in range(len(liczby1)):
     liczby1[i] = liczby1[i].split()
-    liczby1[i].sort
+    liczby1[i] = [int(x) for x in liczby1[i]]
+    liczby1[i].sort()
 
 for i in range(len(liczby2)):
     liczby2[i] = liczby2[i].split()
-    liczby2[i].sort
+    liczby2[i] = [int(x) for x in liczby2[i]]
+    liczby2[i].sort()
+
+print(liczby1)
+print(liczby2)
 
 liczby_ale_posortowane = []
 
+for i in range(len(liczby1)):
+    a = b = 0
+    while a < len(liczby1[i]) and b < len(liczby2[i]):
+        if liczby1[i][a] < liczby2[i][b]:
+            liczby_ale_posortowane.append(liczby1[i][a])
+            a += 1
+        else:
+            liczby_ale_posortowane.append(liczby2[i][b])
+            b += 1
+    liczby_ale_posortowane += liczby1[i][a:]
+    liczby_ale_posortowane += liczby2[i][b:]
+        
+print(liczby_ale_posortowane)
+'''
 for i in range(1000):
     for j in range(10):
         if liczby1[i][j] <= liczby2[i][j]:
             liczby_ale_posortowane.append(liczby1[i][j])
-        else:
+            liczby1[i].remove([j])
+        elif liczby1[i][j] >= liczby2[i][j]:
             liczby_ale_posortowane.append(liczby2[i][j])
-    
-print(liczby_ale_posortowane)
+            liczby2[i].remove([j])
 
+print(liczby_ale_posortowane)
+'''
 file1.close()
 file2.close()
